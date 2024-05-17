221de63c466ed5d68b62cd679dcc341fa397cb58
ThavinduUshan <thavinduushan@gmail.com>
2024-05-17 16:03:43 +0530

    Domain classes added

---
+++ api/Models/MoodEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api;
+
+public class MoodEntry
+{
+    public int Id { get; set; }
+    [Required]
+    public float Score { get; set; }
+    public string Note { get; set; } = string.Empty;
+    public DateTime AddedAt { get; set; } = DateTime.Now;
+
+}
---
+++ api/Models/MoodSchedule.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api;
+
+public class MoodSchedule
+{
+    public int Id { get; set; }
+    [Required]
+    public DateTime ScheduledAt { get; set; }
+
+}
